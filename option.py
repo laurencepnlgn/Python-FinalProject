659cd9cd40319338c2d06241391c2bbabdd2a7a2
SerranoLaica <113954027+SerranoLaica@users.noreply.github.com>
2022-12-05 19:20:47 +0800

    Update option.py

--- option.py
+++ option.py
@@ -362,4 +362,34 @@ class OrderProduct():
                 print("     Total Price: ", total)
                 print(" ------------------------------------")
                 customer_list = " ".join(["Customer Name:", customer_name, "Item:",item, "Amount:", str(amount), "Total Price:", str(total)])
-                self.data.append(customer_list)               
+                self.data.append(customer_list) 
+               
+        elif product == "0612" or product == "PSU":
+            item = "PSU"
+            print("         ".join(self.dp12))
+            print("\n")
+        
+            customer_name = str(input("Enter Customer Name: "))
+            amount = int(input("Order Amount: "))
+            total = amount * 2995
+            
+            print("\n")
+            confirmation = input("Confirm Order [y] | [n]: ")
+            if confirmation == "y" or confirmation == "Y":
+                print("\n")
+                print(" ------------------------------------")
+                print(" |  Computer Parts Ordering System  |")
+                print(" |       Rizal Ave. Bats City       |")
+                print(" |      www.computerpartsos.com     |")
+                print(" |     Welcome to Computer Parts    |")
+                print(" |          Ordering System         |")
+                print(" ------------------------------------")
+                print("     Customer Name: ", customer_name)
+                print(" ------------------------------------")
+                print("     Item: POWER SUPPLY (PSU)")
+                print("     Amount: ", amount)
+                print(" ------------------------------------")
+                print("     Total Price: ", total)
+                print(" ------------------------------------")
+                customer_list = " ".join(["Customer Name:", customer_name, "Item:",item, "Amount:", str(amount), "Total Price:", str(total)])
+                self.data.append(customer_list)          
