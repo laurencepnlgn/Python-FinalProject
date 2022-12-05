e44106e9d8816a2998f93b2f422748d3326124c9
SerranoLaica <113954027+SerranoLaica@users.noreply.github.com>
2022-12-05 19:06:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,46 @@ if __name__ == "__main__":
                 print('| *  Invalid Username or Password. Please Try Again!  * |')
                 print("---------------------------------------------------------\n")
                 login_screen()
+    def login_screen():
+        print("------------------------------------------")
+        print("|       Please login to continue...      |")
+        print("------------------------------------------\n")
+        acc = Login(input(' Enter Username: '), input(' Enter Password: ')) 
+        acc.login()
+        
+    print("\n\n+-------------------------------------------------+")
+    print("|                                                 |")
+    print("|                   WELCOME TO                    |")
+    print("|         COMPUTER PARTS ORDERING SYSTEM          |")
+    print("|                    made by:                     |")
+    print("|            Laurence, Ella, and Laica            |")
+    print("|                                                 |")
+    print("+-------------------------------------------------+\n")
+    
+    login_screen()
+    while True:
+        print("+-----------------------------+")
+        print("|    [1] Display Product      |")
+        print("|    [2] View Order           |")
+        print("|    [3] Exit                 |")
+        print("+-----------------------------+")
+        print("\n")
+        print("--------------------------------------")
+        option = int(input("    Select an option: "))
+        print("--------------------------------------")
+        print("\n")
+        x = OrderProduct()
+        if  option == 1: 
+            x.display()
+            print(" ")
+            x.order_product(input("Order Product:  "))
+            print(" ")
+        elif option == 2:
+            x.viewOrder()
+        elif option == 3:
+            exit()
+        else:
+            print("+-------------------------------------+")
+            print("|           Invalid Input             |")
+            print("+-------------------------------------+")
+            print("")    
