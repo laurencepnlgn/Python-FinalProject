d3f7d30cc187b9699e3e05415fd6582b88c632a9
John Laurence M. Panaligan <113991600+Lrencepnlgn@users.noreply.github.com>
2022-12-12 04:16:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,35 @@
-from option import OrderProduct
+from utils.login import Login
+from utils.signup import Signup
+
 if __name__ == "__main__":
-    class Login():
-        def __init__(self, user, passw):
-            self.user = user
-            self.passw = passw
-    
-        def login(self):
-            if self.user == "admin" and self.passw == "admin123":
-                print("\n-------------------------------")
-                print('| *   Login Successfuly...  * |')
-                print("-------------------------------\n")
-            elif self.user == "ADMIN" and self.passw == "ADMIN123":
-                print("\n-------------------------------")
-                print('| *   Login Successfuly...  * |')
-                print("-------------------------------\n")
-            else:
-                print("\n---------------------------------------------------------")
-                print('| *  Invalid Username or Password. Please Try Again!  * |')
-                print("---------------------------------------------------------\n")
-                login_screen()
-    def login_screen():
-        print("------------------------------------------")
-        print("|       Please login to continue...      |")
-        print("------------------------------------------\n")
-        acc = Login(input(' Enter Username: '), input(' Enter Password: ')) 
-        acc.login()
-        
-    print("\n\n+-------------------------------------------------+")
-    print("|                                                 |")
-    print("|                   WELCOME TO                    |")
-    print("|         COMPUTER PARTS ORDERING SYSTEM          |")
-    print("|                    made by:                     |")
-    print("|            Laurence, Ella, and Laica            |")
-    print("|                                                 |")
-    print("+-------------------------------------------------+\n")
-    
-    login_screen()
     while True:
+        print("\n\n+--------------------------------------------------+")
+        print("|                                                  |")
+        print("|                   WELCOME TO                     |")
+        print("|         COMPUTER PARTS ORDERING SYSTEM           |")
+        print("|                    made by:                      |")
+        print("|            Laurence, Ella, and Laica             |")
+        print("|                                                  |")
+        print("+--------------------------------------------------+\n")
+
         print("+-----------------------------+")
-        print("|    [1] Display Product      |")
-        print("|    [2] View Order           |")
+        print("|    [1] Login                |")
+        print("|    [2] Sign Up              |")
         print("|    [3] Exit                 |")
         print("+-----------------------------+")
         print("\n")
-        print("--------------------------------------")
-        option = int(input("    Select an option: "))
-        print("--------------------------------------")
-        print("\n")
-        x = OrderProduct()
-        if  option == 1: 
-            x.display()
-            print(" ")
-            x.order_product(input("Order Product:  "))
-            print(" ")
-        elif option == 2:
-            x.viewOrder()
-        elif option == 3:
-            exit()
+        try: 
+            choice = int(input("    Select an option: "))
+            print("\n")
+        except ValueError:
+            print("Numbers Only Please.")
         else:
-            print("+-------------------------------------+")
-            print("|           Invalid Input             |")
-            print("+-------------------------------------+")
-            print("")    
+            if choice == 1:
+                login_acc = Login(input("Enter your username: "), input("Enter your password: "))
+                login_acc.check()
+            elif choice == 2:
+                Signup.sign_up_acct()
+            elif choice == 3:
+                exit()
+            else:
+                print("Invalid Input")
